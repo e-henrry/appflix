4b8523bda502056b707a2165c48cac34d304700a
e-henrry <ezequielhenrique806@gmail.com>
2020-07-29 14:24:08 -0300

    Commit dois

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './components/data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414" }}>
       <Menu></Menu>
       <BannerMain 
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo} 
         url={dadosIniciais.categorias[0].videos[0].url} 
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"} /> 
+        videoDescription={"As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial..."} /> 
       <Carousel 
         ignoreFirstVideo category={dadosIniciais.categorias[0]} /> 
       <Carousel 
@@ -30,4 +30,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
